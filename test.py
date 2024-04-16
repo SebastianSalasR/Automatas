cfc25dd67313f21b61a7f3e614c7a6751d1f419b
SebastianSalasR <sebastian_salas_r@hotmail.com>
2024-04-16 17:22:39 -0400

    Codigo trabajo automatas

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 class AutomataFinitoDeterminista:
-	def __init__(self, estados, alfabeto, transiciones, estado_inicial, estados_aceptacion):
+	def __init__(self, estados, alfabeto, transiciones, estado_inicial, estados_finales):
 		self.estados = estados
 		self.alfabeto = alfabeto
 		self.transiciones = transiciones
 		self.estado_inicial = estado_inicial
-		self.estados_aceptacion = estados_aceptacion
+		self.estados_finales = estados_finales
 
 	def transicion(self, estado, simbolo):
 		return self.transiciones.get((estado, simbolo))
@@ -15,29 +15,30 @@ class AutomataFinitoDeterminista:
 			estado_actual = self.transicion(estado_actual, simbolo)
 			if estado_actual is None:
 				return False
-		return estado_actual in self.estados_aceptacion
+		return estado_actual in self.estados_finales
 
 
 def main():
-	# Definir los componentes del autómata
-	estados = {'q0', 'q1', 'q2'}
-	alfabeto = {'0', '1'}
-	transiciones = {('q0', '0'): 'q1', ('q0', '1'): 'q0', ('q1', '0'): 'q2', ('q1', '1'): 'q0', ('q2', '0'): 'q2', ('q2', '1'): 'q2'}
-	estado_inicial = 'q0'
-	estados_aceptacion = {'q1'}
+
+	estados = {"q0", "q1", "q2"}
+	alfabeto = {"0", "1"}
+	transiciones = {("q0", "0"): "q1", ("q0", "1"): "q0", ("q1", "0"): "q2", ("q1", "1"): "q0", ("q2", "0"): "q2", ("q2", "1"): "q2"}
+	estado_inicial = "q0"
+	estados_finales = {"q1"}
 
 	# Crear el autómata finito determinista
-	afd = AutomataFinitoDeterminista(estados, alfabeto, transiciones, estado_inicial, estados_aceptacion)
+	afd = AutomataFinitoDeterminista(estados, alfabeto, transiciones, estado_inicial, estados_finales)
 
 	# Ejemplo de cadena a evaluar
-	cadena = '1010101'
+	cadena = "1010101"
 
 	# Verificar si la cadena es aceptada por el autómata
-	if afd.acepta_cadena(cadena):
-		print(f'La cadena "{cadena}" es aceptada por el autómata.')
+	if (afd.acepta_cadena(cadena)):
+		print("La cadena " + cadena + "es aceptada por el automata")
 	else:
-		print(f'La cadena "{cadena}" no es aceptada por el autómata.')
+		print("La cadena " + cadena + "no es aceptada por el automata")
 
 
-if __name__ == "__main__":
+
+if (__name__ == "__main__"):
 	main()
